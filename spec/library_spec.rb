dbbdbbb24f1a26d47c829b11df071b105f9b9481
Reid Miller <reid.s.miller@gmail.com>
2023-02-24 10:39:15 -0700

    test: Add tests for iteration 4

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -42,4 +42,61 @@ RSpec.describe Library do
       expect(library.publication_time_frame_for(james_corey)).to eq({:start=>"2011", :end=>"2021"})
     end
   end
+
+  describe '#Iteration 4' do
+    it 'can checkout a book' do
+      library = Library.new("Denver Public Library")
+      charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+      jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+      villette = charlotte_bronte.write("Villette", "October 12, 1853")
+      james_corey = Author.new({first_name: "James", last_name: "Corey"})
+      leviathan_wakes = james_corey.write("Leviathan Wakes", "June 15, 2011")
+      leviathan_falls = james_corey.write("Leviathan Falls", "November 30, 2021")
+      nemesis_games = james_corey.write("Nemesis Games", "June 2, 2015")
+
+      expect(librar.books_checked_out).to eq([])
+
+      library.checkout(leviathan_wakes)
+      expect(library.books_checked_out).to eq([leviathan_wakes])
+    end
+
+    it 'can return a book' do
+      library = Library.new("Denver Public Library")
+      charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+      jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+      villette = charlotte_bronte.write("Villette", "October 12, 1853")
+      james_corey = Author.new({first_name: "James", last_name: "Corey"})
+      leviathan_wakes = james_corey.write("Leviathan Wakes", "June 15, 2011")
+      leviathan_falls = james_corey.write("Leviathan Falls", "November 30, 2021")
+      nemesis_games = james_corey.write("Nemesis Games", "June 2, 2015")
+
+      library.checkout(leviathan_wakes)
+      library.checkout(jane_eyre)
+
+      expect(library.books_checked_out).to eq([leviathan_wakes, jane_eyre])
+
+      library.return(leviathan_wakes)
+
+      expect(library.books_checked_out).to eq([jane_eyre])
+    end
+
+    it 'can find a book that has been checked out the most times' do
+      library = Library.new("Denver Public Library")
+      charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+      jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+      villette = charlotte_bronte.write("Villette", "October 12, 1853")
+      james_corey = Author.new({first_name: "James", last_name: "Corey"})
+      leviathan_wakes = james_corey.write("Leviathan Wakes", "June 15, 2011")
+      leviathan_falls = james_corey.write("Leviathan Falls", "November 30, 2021")
+      nemesis_games = james_corey.write("Nemesis Games", "June 2, 2015")
+
+      library.checkout(leviathan_wakes)
+      library.checkout(jane_eyre)
+      library.return(leviathan_wakes)
+      library.checkout(leviathan_wakes)
+
+      expect(leviathan_wakes.checkouts).to eq(2)
+      expect(library.most_checked_out).to eq(leviathan_wakes)
+    end
+  end
 end

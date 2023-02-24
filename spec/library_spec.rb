4e5507d4c3ed0c6124cec56d80331aea6e3f4108
Reid Miller <reid.s.miller@gmail.com>
2023-02-24 09:55:47 -0700

    test: Add test for adding authors and all their books

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -7,7 +7,19 @@ RSpec.describe Library do
 
       expect(library.name).to eq("Denver Public Library")
       expect(library.books).to eq([])
-      expect(library.authors).to eq({})
+      expect(library.authors).to eq([])
+    end
+
+    it 'can add authors and all their books' do
+      library = Library.new("Denver Public Library")
+      charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+      jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+      villette = charlotte_bronte.write("Villette", "1853")
+
+      library.add_author(charlotte_bronte)
+
+      expect(library.authors).to eq([charlotte_bronte])
+      expect(library.books).to eq([jane_eyre, villette])
     end
   end
 end

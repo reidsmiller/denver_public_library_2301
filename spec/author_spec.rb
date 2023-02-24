42f05012a2d1b9379979031f2be10f47ea7fcb23
Reid Miller <reid.s.miller@gmail.com>
2023-02-24 09:40:30 -0700

    test: Add multiple books stored in array test

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -17,5 +17,13 @@ RSpec.describe Author do
       expect(jane_eyre.class).to eq(Book)
       expect(jane_eyre.title).to eq("Jane Eyre")
     end
+
+    it 'can store multiple books in array' do
+      charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+      jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+      villette = charlotte_bronte.write("Villette", "1853")
+
+      expect(charlotte_bronte.books).to eq([jane_eyre, villette])
+    end
   end
 end

da6820112b81b81b30583d006d7993673c5c508c
Reid Miller <reid.s.miller@gmail.com>
2023-02-24 10:29:11 -0700

    feat: Add publication time frame for method

--- lib/library.rb
+++ lib/library.rb
@@ -8,6 +8,19 @@ class Library
   end
 
   def add_author(author)
-    
+    @authors << author
+    author.books.each {|book| @books << book}
+  end
+
+  def publication_time_frame_for(author)
+    publication_years = []
+    author.books.each {|book| publication_years << book.publication_year}
+    publication_years_i = []
+    publication_years.each {|year| publication_years_i << year.to_i}
+    publication_years.sort!
+    time_frame = {
+      start: publication_years.first.to_s,
+      end: publication_years.last.to_s
+    }
   end
 end

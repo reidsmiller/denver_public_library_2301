ee6b18b9bb831d2472f369a5b1117c56b8aaed29
Reid Miller <reid.s.miller@gmail.com>
2023-02-24 09:17:07 -0700

    feat: Add Book class

--- lib/book.rb
+++ lib/book.rb
@@ -1,3 +1,10 @@
 class Book
-  
+  attr_reader :author_first_name, :author_last_name, :title, :publication_date
+
+  def initialize(book_info)
+    @author_first_name = book_info[:author_first_name]
+    @author_last_name = book_info[:author_last_name]
+    @title = book_info[:title]
+    @publication_date = book_info[:publication_date]
+  end
 end
--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-Rspec.describe Book do
+RSpec.describe Book do
   describe '#Iteration 1' do
     it 'can instantiate new book' do
       book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
@@ -10,7 +10,5 @@ Rspec.describe Book do
       expect(book.title).to eq ("To Kill a Mockingbird")
       expect(book.publication_date).to eq("July 11, 1960")
     end
-
-    
   end
 end

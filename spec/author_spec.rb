ff7d31573811f0a66039082c058b529243a14334
Reid Miller <reid.s.miller@gmail.com>
2023-02-24 09:38:20 -0700

    test: Add test for creating bookunder author name

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -9,5 +9,13 @@ RSpec.describe Author do
       expect(charlotte_bronte.last_name).to eq("Bronte")
       expect(charlotte_bronte.books).to eq([])
     end
+
+    it 'can create book under author name' do
+      charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+      jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+
+      expect(jane_eyre.class).to eq(Book)
+      expect(jane_eyre.title).to eq("Jane Eyre")
+    end
   end
 end

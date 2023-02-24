c3a1599835398b829a3a521f62b27e087a5b52f0
Reid Miller <reid.s.miller@gmail.com>
2023-02-24 09:36:57 -0700

    feat: Add author class"

--- lib/author.rb
+++ lib/author.rb
@@ -1,3 +1,9 @@
 class Author
+  attr_reader :first_name, :last_name, :books
 
+  def initialize(author_info)
+    @first_name = author_info[:first_name]
+    @last_name = author_info[:last_name]
+    @books = []
+  end
 end

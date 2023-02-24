cdccbd81988da7ccb72520fbec1d0d575d534f09
Reid Miller <reid.s.miller@gmail.com>
2023-02-24 10:04:06 -0700

    test: Add publication time frame for test

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -15,11 +15,30 @@ RSpec.describe Library do
       charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
       jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
       villette = charlotte_bronte.write("Villette", "1853")
+      james_corey = Author.new({first_name: "James", last_name: "Corey"})
+      leviathan_wakes = james_corey.write("Leviathan Wakes", "June 15, 2011")
+      nemesis_games = james_corey.write("Nemesis Games", "June 2, 2015")
+      leviathan_falls = james_corey.write("Leviathan Falls", "November 30, 2021")
+
 
       library.add_author(charlotte_bronte)
 
-      expect(library.authors).to eq([charlotte_bronte])
-      expect(library.books).to eq([jane_eyre, villette])
+      expect(library.authors).to eq([charlotte_bronte, james_corey])
+      expect(library.books).to eq([jane_eyre, villette, leviathan_wakes, nemesis_games, leviathan_falls])
+    end
+
+    it 'can tell us publication time frame for author' do
+      library = Library.new("Denver Public Library")
+      charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+      jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+      villette = charlotte_bronte.write("Villette", "1853")
+      james_corey = Author.new({first_name: "James", last_name: "Corey"})
+      leviathan_wakes = james_corey.write("Leviathan Wakes", "June 15, 2011")
+      nemesis_games = james_corey.write("Nemesis Games", "June 2, 2015")
+      leviathan_falls = james_corey.write("Leviathan Falls", "November 30, 2021")
+
+      expect(library.publication_time_frame_for(charlotte_bronte)).to eq({:start=>"1847", :end=>"1853"})
+      expect(library.publication_time_frame_for(james_corey)).to eq({:start=>"2011", :end=>"2021"})
     end
   end
 end
